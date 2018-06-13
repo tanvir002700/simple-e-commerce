f6a5f221c990f1a8bee8b8e4cf634d755a423f6e
tanvir002700 <tanvir002700@gmail.com>
2018-06-13 13:46:05 +0600

    add store controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
   resources :products
+  resources :store
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :home, only: [:index]
