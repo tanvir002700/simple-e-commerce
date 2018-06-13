f434e3fbe42672bf836d93236f457198aee0021d
tanvir hasan <tanvir002700@gmail.com>
2018-06-14 03:18:52 +0600

    refactor admin product controller

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -17,35 +17,24 @@ module Admin
     def create
       @product = Product.new(product_params)
 
-      respond_to do |format|
-        if @product.save
-          format.html { redirect_to [:admin, @product], notice: 'Product was successfully created.' }
-          format.json { render :show, status: :created, location: @product }
-        else
-          format.html { render :new }
-          format.json { render json: @product.errors, status: :unprocessable_entity }
-        end
+      if @product.save
+        redirect_to [:admin, @product], notice: 'Product was successfully created.'
+      else
+        render :new
       end
     end
 
     def update
-      respond_to do |format|
         if @product.update(product_params)
-          format.html { redirect_to [:admin, @product], notice: 'Product was successfully updated.' }
-          format.json { render :show, status: :ok, location: @product }
+          redirect_to [:admin, @product], notice: 'Product was successfully updated.'
         else
-          format.html { render :edit }
-          format.json { render json: @product.errors, status: :unprocessable_entity }
+          render :edit
         end
-      end
     end
 
     def destroy
       @product.destroy
-      respond_to do |format|
-        format.html { redirect_to admin_products_url, notice: 'Product was successfully destroyed.' }
-        format.json { head :no_content }
-      end
+      redirect_to admin_products_url, notice: 'Product was successfully destroyed.'
     end
 
     private
