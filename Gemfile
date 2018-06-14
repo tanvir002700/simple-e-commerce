bc74f1b4e39320c93b791c65e945db4a410b72a7
tanvir002700 <tanvir002700@gmail.com>
2018-06-14 12:44:41 +0600

    migrate db to postgresql

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'tzinfo-data', platforms: %I[mingw mswin x64_mingw jruby]
 gem 'uglifier', '>= 1.3.0'
 gem 'validates_email_format_of', '~> 1.6', '>= 1.6.3'
-gem 'mysql2', '~> 0.3.18'
+gem 'pg'
 
 group :development do
   gem 'better_errors'
--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,15 @@
 
 ActiveRecord::Schema.define(version: 20180613203714) do
 
-  create_table "carts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "line_items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "line_items", force: :cascade do |t|
     t.bigint "product_id"
     t.bigint "cart_id"
     t.datetime "created_at", null: false
@@ -28,7 +31,7 @@ ActiveRecord::Schema.define(version: 20180613203714) do
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
 
-  create_table "orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "orders", force: :cascade do |t|
     t.string "name"
     t.text "address"
     t.string "email"
@@ -40,7 +43,7 @@ ActiveRecord::Schema.define(version: 20180613203714) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
-  create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "products", force: :cascade do |t|
     t.string "title"
     t.string "img_url"
     t.integer "price"
@@ -48,7 +51,7 @@ ActiveRecord::Schema.define(version: 20180613203714) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
