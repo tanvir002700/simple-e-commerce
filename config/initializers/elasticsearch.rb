c32f959b569f7b191a6b7237e94c3587b8caee5c
tanvir002700 <tanvir002700@gmail.com>
2018-06-14 16:32:41 +0600

    update elasticsearch configuration

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,1 +1,18 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new(url: 'http://elasticsearch:9200')
+ELASTICSEARCH_CONFIG = Proc.new do
+  host = ENV['ELASTIC_HOST']
+  port = ENV['ELASTIC_PORT']
+  puts ENV
+
+  elastic_ready = host.present? and port.present?
+  puts "###############33#### elastic ready #{elastic_ready} #{host.present?} #{port.present?}"
+  elasticsearch_url =
+    if elastic_ready
+      "http://#{host}:#{port}"
+    else
+      "http://localhost:9200"
+    end
+  elasticsearch_url
+end
+
+puts ELASTICSEARCH_CONFIG.call
+Elasticsearch::Model.client = Elasticsearch::Client.new(url: ELASTICSEARCH_CONFIG.call)
