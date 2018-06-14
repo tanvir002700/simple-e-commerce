39ec9a3f59f21f387c192bebde257320515c240f
tanvir hasan <tanvir002700@gmail.com>
2018-06-14 23:19:04 +0600

    refactor elasticsearch config

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,10 +1,8 @@
 ELASTICSEARCH_CONFIG = Proc.new do
   host = ENV['ELASTIC_HOST']
   port = ENV['ELASTIC_PORT']
-  puts ENV
 
   elastic_ready = host.present? and port.present?
-  puts "###############33#### elastic ready #{elastic_ready} #{host.present?} #{port.present?}"
   elasticsearch_url =
     if elastic_ready
       "http://#{host}:#{port}"
@@ -14,5 +12,4 @@ ELASTICSEARCH_CONFIG = Proc.new do
   elasticsearch_url
 end
 
-puts ELASTICSEARCH_CONFIG.call
 Elasticsearch::Model.client = Elasticsearch::Client.new(url: ELASTICSEARCH_CONFIG.call)
