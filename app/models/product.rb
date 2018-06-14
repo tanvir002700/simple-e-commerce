d68599126e26afd2a171808c74e2684182205b81
tanvir002700 <tanvir002700@gmail.com>
2018-06-14 11:13:15 +0600

    implement elasticsearch for store product search

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
+  require 'elasticsearch/model'
+
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
   has_many :line_items, dependent: :destroy
 end
