1ecd3939fc560c8984c4edb3e275865b3e7b9aa6
tanvir hasan <tanvir002700@gmail.com>
2018-06-14 23:40:10 +0600

    add cart empty action

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,10 @@
 class CartsController < ApplicationController
   def show; end
+
+  def destroy
+    @cart.destroy
+    respond_to do |format|
+      format.html { redirect_to orders_url, notice: 'Cart was successfully destroyed.' }
+    end
+  end
 end
