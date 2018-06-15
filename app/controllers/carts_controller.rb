fe0d1655fea7e91157467daf9b66f0a8fb062455
tanvir hasan <tanvir002700@gmail.com>
2018-06-15 16:19:16 +0600

    update cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,6 +3,6 @@ class CartsController < ApplicationController
 
   def destroy
     @cart.destroy
-    redirect_to orders_url, notice: 'Cart was successfully destroyed.'
+    redirect_to store_index_path, notice: 'Cart was successfully destroyed.'
   end
 end
