5ab1fa3bf9ba9e37987ef4bc30ea0cf6fc0bf540
tanvir hasan <tanvir002700@gmail.com>
2018-06-15 13:18:04 +0600

    add pagination for store product

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
   def index
-    @products = Product.search((params[:q].present? ? params[:q] : '*')).records
+    @products = Product.search((params[:q].present? ? params[:q] : '*'))
+                       .records.paginate(page: params[:page], per_page: 10)
+
   end
 end
